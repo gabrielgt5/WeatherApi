2f3dfe7191cc232d66eef33c68acb49696736f82
gabrielgt5 <lucasgabrielgt758@gmail.com>
2022-09-21 21:09:50 -0400

    Adicoinando novas funcionalidade

---
+++ script.js
@@ -0,0 +1,15 @@
+//buscar latitude e longitude com api do google;
+const buscar = document.getElementById('buscar').addEventListener('click', foi);
+const cidade = document.getElementById('cidade');
+
+
+function foi(){
+    alert(cidade.value)
+}
+
+function showPosition(ci){
+
+}
+
+//chave api maps:AIzaSyBpPQqHexFT35Onxezn8ZmaQDPMGT74v_E
+//https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBpPQqHexFT35Onxezn8ZmaQDPMGT74v_E
